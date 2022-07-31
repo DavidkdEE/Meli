d22cb7f5a3607824dd88bf08889dbce88fb4203b
kdEE <kde9999@gmail.com>
2022-07-31 10:36:44 -0400

    ✅ Add unauthorizated test

--- api_meli/tests/core/test_api.py
+++ api_meli/tests/core/test_api.py
@@ -47,6 +47,7 @@ class ApiTest(TestCase):
     @patch('api_google.credentials.GoogleDrive.get_credentials', fake_get_creds)
     def test_create_file(self):
         """Creation doc test"""
+
         payload = {
             "title": fake.name(),
             "description": fake.text(),
@@ -66,6 +67,7 @@ class ApiTest(TestCase):
 
     def test_create_file_unauthorizated(self):
         """Creation doc unauthorizated test"""
+
         payload = {
             "title": fake.name(),
             "description": fake.text(),
@@ -81,6 +83,7 @@ class ApiTest(TestCase):
     @patch('api_google.credentials.GoogleDrive.get_credentials', fake_get_creds)
     def test_get_files(self):
         """Creation get list test"""
+
         authorization = 'Bearer ' + self.token
         response = self.client.get(
             reverse('files'),
@@ -91,11 +94,20 @@ class ApiTest(TestCase):
         if response.data:
             for file in response.data:
                 ListFilesResponse(**file)
+    
+    def test_get_files_unauthorizated(self):
+        """Get list unauthorizated test"""
 
+        response = self.client.get(
+            reverse('files'),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+  
     @patch('api_google.google_drive.GoogleDriveService.delete_file', fake_delete_file)
     @patch('api_google.credentials.GoogleDrive.get_credentials', fake_get_creds)
     def test_delete_file(self):
-        """Creation get list test"""
+        """Delete file test"""
         authorization = 'Bearer ' + self.token
         response = self.client.delete(
             reverse('delete-file', kwargs={'id': 'id_file'}),
@@ -103,6 +115,15 @@ class ApiTest(TestCase):
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    
+    def test_delete_file_unauthorizated(self):
+        """Delete file unauthorizated test"""
+
+        response = self.client.delete(
+            reverse('delete-file', kwargs={'id': 'id_file'}),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     @patch('api_google.google_drive.GoogleDriveService.create_file', fake_create_file)
     @patch('api_google.google_drive.GoogleDriveService.update_file', fake_update_file)
@@ -145,3 +166,14 @@ class ApiTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(response.data.get('detail'), 'La palabra no se encuentra en el texto.')
 
+    def test_search_in_file_unauthorizated(self):
+        """Search in doc unauthorizated test"""
+        
+        id_file = 'id_file'
+        data = {'word': 'description'}
+        response = self.client.get(
+            reverse('search-in-doc', kwargs = {"id": id_file}),
+            data,
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
